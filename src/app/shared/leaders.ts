da7cb36a62a000511469d6b0422a73c3d1a4eed4
udaykumarkotla <kudaykumar.18.cse@anits.edu.in>
2021-12-22 20:08:32 +0530

    changes

--- src/app/shared/leaders.ts
+++ src/app/shared/leaders.ts
@@ -3,7 +3,7 @@ export const LEADERS :Leader[]=[
     {
       id: '0',
       name: 'Peter Pan',
-      image: '/assets/images/alberto.png',
+      image: 'assets/images/alberto.png',
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
@@ -13,7 +13,7 @@ export const LEADERS :Leader[]=[
     {
       id: '1',
       name: 'Dhanasekaran Witherspoon',
-      image: '/assets/images/alberto.png',
+      image: 'assets/images/alberto.png',
       designation: 'Chief Food Officer',
       abbr: 'CFO',
       featured: false,
@@ -23,7 +23,7 @@ export const LEADERS :Leader[]=[
     {
       id: '2',
       name: 'Agumbe Tang',
-      image: '/assets/images/alberto.png',
+      image: 'assets/images/alberto.png',
       designation: 'Chief Taste Officer',
       abbr: 'CTO',
       featured: false,
@@ -33,7 +33,7 @@ export const LEADERS :Leader[]=[
     {
       id: '3',
       name: 'Alberto Somayya',
-      image: '/assets/images/alberto.png',
+      image: 'assets/images/alberto.png',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
